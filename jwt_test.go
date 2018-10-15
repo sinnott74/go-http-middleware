cd03c544552cd8bc0bbed2a6e26078b88331b9b4
Daniel Sinnott <sinnott74@hotmail.com>
2018-10-15 13:08:59 +0100

    Allow custom JWT authorisation scheme. Setting default to JWT {token}

--- jwt_test.go
+++ jwt_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 
@@ -60,7 +61,7 @@ func TestJWTValidToken(t *testing.T) {
 	// Arrange
 	secret := []byte("SECRET_SSSHHHHHHH")
 	jwtOptions := JWTOptions{Secret: secret}
-	token := createValidJWT(t, secret)
+	token := createValidJWT(t, secret, "JWT")
 	r, _ := http.NewRequest("GET", "/", nil)
 	r.Header.Add("Authorization", token)
 	w := httptest.NewRecorder()
@@ -84,7 +85,7 @@ func TestJWTExpiredToken(t *testing.T) {
 	secret := []byte("SECRET_SSSHHHHHHH")
 	jwtOptions := JWTOptions{Secret: secret}
 	r, _ := http.NewRequest("GET", "/", nil)
-	token := createJWTWithExpiration(t, secret, time.Now().Add(-time.Minute*1))
+	token := createJWTWithExpiration(t, secret, "JWT", time.Now().Add(-time.Minute*1))
 	r.Header.Add("Authorization", token)
 	w := httptest.NewRecorder()
 	auth := JWT(jwtOptions)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -108,7 +109,7 @@ func TestJWTValidTokenWithUserSuppliedFunc(t *testing.T) {
 		userCtx := context.WithValue(ctx, userContextKey, "test@test.com")
 		return userCtx, nil
 	}}
-	token := createValidJWT(t, secret)
+	token := createValidJWT(t, secret, "JWT")
 	r, _ := http.NewRequest("GET", "/", nil)
 	r.Header.Add("Authorization", token)
 	w := httptest.NewRecorder()
@@ -135,7 +136,7 @@ func TestJWTValidTokenWithUserSuppliedFuncThatReturnsError(t *testing.T) {
 	jwtOptions := JWTOptions{Secret: secret, AuthFunc: func(ctx context.Context, claims jwt.MapClaims) (context.Context, error) {
 		return ctx, errors.New("User supplied func says claims aren't good")
 	}}
-	token := createValidJWT(t, secret)
+	token := createValidJWT(t, secret, "JWT")
 	r, _ := http.NewRequest("GET", "/", nil)
 	r.Header.Add("Authorization", token)
 	w := httptest.NewRecorder()
@@ -152,21 +153,85 @@ func TestJWTValidTokenWithUserSuppliedFuncThatReturnsError(t *testing.T) {
 	}
 }
 
-func createValidJWT(t *testing.T, secret []byte) string {
+func TestJWTValidTokenWithCustomTokenExtractor(t *testing.T) {
+
+	// Arrange
+	secret := []byte("SECRET_SSSHHHHHHH")
+	jwtOptions := JWTOptions{
+		Secret: secret,
+		AuthFunc: func(ctx context.Context, claims jwt.MapClaims) (context.Context, error) {
+			return ctx, errors.New("User supplied func says claims aren't good")
+		},
+		Extractor: func(authHeaderValue string) (string, error) {
+			authHeaderParts := strings.Split(authHeaderValue, " ")
+			if len(authHeaderParts) != 2 || strings.ToLower(authHeaderParts[0]) != "bearer" {
+				return "", errors.New("Authorization header format must be bearer {token}")
+			}
+			return authHeaderParts[1], nil
+		},
+	}
+	token := createValidJWT(t, secret, "bearer")
+	r, _ := http.NewRequest("GET", "/", nil)
+	r.Header.Add("Authorization", token)
+	w := httptest.NewRecorder()
+	auth := JWT(jwtOptions)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t.Fatal("Next handler should not have been called are user supplied func returns an error")
+	}))
+
+	// Act
+	auth.ServeHTTP(w, r)
+
+	// Assert
+	if w.Code != http.StatusUnauthorized {
+		t.Fatalf("StatusUnauthorized 401 expected but was %v", w.Code)
+	}
+}
+
+func TestJWTValidTokenWithCustomTokenExtractorError(t *testing.T) {
+
+	// Arrange
+	secret := []byte("SECRET_SSSHHHHHHH")
+	jwtOptions := JWTOptions{
+		Secret: secret,
+		AuthFunc: func(ctx context.Context, claims jwt.MapClaims) (context.Context, error) {
+			return ctx, errors.New("User supplied func says claims aren't good")
+		},
+		Extractor: func(authHeaderValue string) (string, error) {
+			return "", errors.New("Some error getting token from header value")
+		},
+	}
+	token := createValidJWT(t, secret, "bearer")
+	r, _ := http.NewRequest("GET", "/", nil)
+	r.Header.Add("Authorization", token)
+	w := httptest.NewRecorder()
+	auth := JWT(jwtOptions)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t.Fatal("Next handler should not have been called are user supplied func returns an error")
+	}))
+
+	// Act
+	auth.ServeHTTP(w, r)
+
+	// Assert
+	if w.Code != http.StatusUnauthorized {
+		t.Fatalf("StatusUnauthorized 401 expected but was %v", w.Code)
+	}
+}
+
+func createValidJWT(t *testing.T, secret []byte, scheme string) string {
 	claims := jwt.MapClaims{}
 	tokenString, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString(secret)
 	if err != nil {
 		t.Fatal(err)
 	}
-	return tokenString
+	return scheme + " " + tokenString
 }
 
-func createJWTWithExpiration(t *testing.T, secret []byte, expiration time.Time) string {
+func createJWTWithExpiration(t *testing.T, secret []byte, scheme string, expiration time.Time) string {
 	claims := jwt.MapClaims{}
 	claims["exp"] = expiration.Unix()
 	tokenString, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString(secret)
 	if err != nil {
 		t.Fatal(err)
 	}
-	return tokenString
+	return scheme + " " + tokenString
 }
